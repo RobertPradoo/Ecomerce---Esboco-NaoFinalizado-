92709076b00cc76fa1b0d357ac22c9617fb5d1c1
RobertPradoo <146465514+RobertPradoo@users.noreply.github.com>
2024-04-02 22:02:46 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,6 @@ import produto from './produto-1.png';
 
 
 function App() {
-  const not = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
   return (
     
     <div className="App">
